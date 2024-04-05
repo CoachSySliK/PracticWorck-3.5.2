6484a29a2bf9089848068f4c57f74e20c34d6c22
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-05 04:58:14 +0300

    Добавьте файлы проекта.

---
+++ PracticWorck-3.5.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 47;
+    int b = 93;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "------\n";
+    int c;
+    c = a;
+    a = b;
+    b = c;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    
+    return 0;
+}
+
